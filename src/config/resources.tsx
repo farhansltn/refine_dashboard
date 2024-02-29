edea352d27fb443b088553808bb1967ed395f700
farhansltn <farhanfarsul@gmail.com>
2024-02-29 14:08:59 +0700

    initial commit

---
+++ src/config/resources.tsx
@@ -0,0 +1,36 @@
+
+import { BankFilled, FileFilled, HomeFilled } from "@ant-design/icons";
+import { IResourceItem } from "@refinedev/core";
+
+export const resources: IResourceItem[]=[
+    {
+        name: 'dashboard',
+        list: '/',
+        meta: {
+            label:'Dashoard',
+            icon:<HomeFilled />
+        }
+    },
+    {
+        name:'companies',
+        list: '/companies',
+        show: '/companies/:id',
+        create: '/companies/new',
+        edit:'/companies/edit/:id',
+        meta: {
+            label:'Companies',
+            icon: <BankFilled />
+        }
+    },
+    {
+        name:'tasks',
+        list: '/tasks',
+        show: '/tasks/:id',
+        create: '/tasks/new',
+        edit:'/tasks/edit/:id',
+        meta: {
+            label:'Tasks',
+            icon: <FileFilled />
+        }
+    }
+]
